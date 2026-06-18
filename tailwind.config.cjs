aa9269b4ebe58824f7ad66fe59c41d639bed4a2f
AnsariShoes <zaif.ansari0010@gmail.com>
2026-06-18 15:29:41 +0530

    Add files via upload

---
+++ tailwind.config.cjs
@@ -0,0 +1,57 @@
+
+module.exports = {
+  content: [./index.html, ./src/**/*.js],
+  theme: {
+    extend: {
+      colors: {
+        primary: #e4487a,
+        secondary: #8a2be2,
+        accent: #ffcc00
+      }
+    }
+  },
+  plugins: [require(tailwindcss/plugin)],
+}
+ 
+module.exports = {
+  content: [./index.html, ./src/**/*.jsx],
+  theme: {
+    extend: {
+      colors: {
+        primary: #e4487a,
+        secondary: #8a2be2,
+        accent: #ffcc00
+      }
+    }
+  },
+  plugins: [require(tailwindcss/plugin)],
+}
+ 
+module.exports = {
+  content: [./index.html, ./src/**/*.ts],
+  theme: {
+    extend: {
+      colors: {
+        primary: #e4487a,
+        secondary: #8a2be2,
+        accent: #ffcc00
+      }
+    }
+  },
+  plugins: [require(tailwindcss/plugin)],
+}
+ 
+module.exports = {
+  content: [./index.html, ./src/**/*.tsx],
+  theme: {
+    extend: {
+      colors: {
+        primary: #e4487a,
+        secondary: #8a2be2,
+        accent: #ffcc00
+      }
+    }
+  },
+  plugins: [require(tailwindcss/plugin)],
+}
+
